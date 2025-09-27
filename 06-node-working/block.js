3a4d69d4e9b6f0162ffee1333be916589c69c1d8
Akshay More <moreakshay725@gmail.com>
2025-09-28 00:29:21 +0530

    refactor: update file reading methods to use non-blocking approach

--- 06-node-working/block.js
+++ 06-node-working/block.js
@@ -6,22 +6,24 @@ const fs = require("fs");
 // console.log("OPERATION DONE");
 
 // Blocking elements
+// console.log("1");
+// const res = fs.readFileSync("./test.txt", "utf-8");
+// console.log(res);
+
+// console.log("2");
+
+// console.log("3");
+// console.log("4");
+
+// Non-Blocking elements
 console.log("1");
-const res = fs.readFileSync("./test.txt", "utf-8");
-console.log(res);
+const res = fs.readFile("./test.txt", "utf-8", (err, res) => {
+  console.log(res);
+});
 
 console.log("2");
-const res2 = fs.readFileSync("./test.txt", "utf-8");
-console.log(res2);
+// const res2 = fs.readFileSync("./test.txt", "utf-8");
+// console.log(res2);
 
 console.log("3");
-const res3 = fs.readFileSync("./test.txt", "utf-8");
-console.log(res3);
-
 console.log("4");
-const res4 = fs.readFileSync("./test.txt", "utf-8");
-console.log(res4);
-
-console.log("5");
-const res5 = fs.readFileSync("./test.txt", "utf-8");
-console.log(res5);

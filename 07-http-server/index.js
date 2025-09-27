06ce72e7a449357ff065340c850705672ca85137
Akshay More <moreakshay725@gmail.com>
2025-09-28 01:14:31 +0530

    feat: log requests to a file in the HTTP server

--- 07-http-server/index.js
+++ 07-http-server/index.js
@@ -1,8 +1,11 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log("request recieved");
-  res.end("Hello from http server");
+  const log = `${Date.now()}: Req received\n`;
+  fs.appendFile("logs.txt", log, (err, data) => {
+    res.end("Hello from http server");
+  });
 });
 
 const port = 5000;

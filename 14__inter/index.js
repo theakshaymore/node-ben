c240ed0ef6524c222e8a560447310f098c7480a0
Akshay More <moreakshay725@gmail.com>
2025-10-08 19:13:24 +0530

    inter setup

---
+++ 14__inter/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const PORT = 3000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to homepage");
+});
+
+app.get("/data", async (req, res) => {
+  try {
+    const response = await fetch("https://akshaymore.netlify.app/rest.json");
+    const data = await response.json();
+    console.log(data);
+    res.status(200).json(data);
+  } catch (err) {
+    console.error("Error fetching data:", err);
+    res.status(500).json({ err: "Failed to fetch data" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`SERVER IS LISTENING ON PORT ${PORT}....`);
+});

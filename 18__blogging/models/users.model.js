e7a361a793a54de3cbf8ec782c9a666e4212ead9
Akshay More <moreakshay725@gmail.com>
2025-11-03 17:20:33 +0530

    EXPRESS: hassedpassword logic

--- 18__blogging/models/users.model.js
+++ 18__blogging/models/users.model.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose");
+const { createHmac } = require("crypto");
 
 const userSchema = new Schema(
   {
@@ -37,6 +38,23 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", function (next) {
+  const user = this;
+
+  if (!user.isModified("password")) return;
+
+  const salt = randomBytes(16).toString();
+
+  const hashedPassword = createHmac("sha256", salt)
+    .update(user.password)
+    .digest("hex");
+
+  this.salt = salt;
+  this.password = hashedPassword;
+
+  next();
+});
 
 const User = model("user", userSchema);
 

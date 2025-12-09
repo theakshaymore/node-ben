6e6dc1929ac5f40bd9ded419478b376a1a1ca56a
Akshay More <moreakshay725@gmail.com>
2025-12-09 11:30:58 +0530

    MONGO: base strucutre of comment schema

--- 18__blogging/models/comment.model.js
+++ 18__blogging/models/comment.model.js
@@ -1,3 +1,34 @@
 import { Schema, model } from "mongoose";
 
-const commentSchema = new Schema({});
+const commentSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+
+    likes: {
+      type: Number,
+      default: 0,
+    },
+
+    disLikes: {
+      type: Number,
+      default: 0,
+    },
+
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
+    blogId: {
+      type: Schema.Types.ObjectId,
+      ref: "blog",
+    },
+  },
+  { timestamps: true }
+);
+
+const Comment = model("comment", commentSchema);
+
+export default Comment;

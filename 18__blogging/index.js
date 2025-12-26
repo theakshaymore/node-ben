b4e924ad22c7bbb2642f0eda236dc2f503912dec
Akshay More <moreakshay725@gmail.com>
2025-12-26 23:14:42 +0530

    chore: update environment variables for PORT and MONGO_URL in index.js

--- 18__blogging/index.js
+++ 18__blogging/index.js
@@ -18,10 +18,8 @@ import {
 const app = express();
 
 // constant variables
-const PORT = 7800;
-// const MONGO_URL = "mongodb://127.0.0.1:27017/everblog";
-// const MONGO_URL = "mongodb://localhost:27017/everblog";
-const MONGO_URL = "mongodb://172.21.128.1:27017/everblog";
+const PORT = process.env.PORT;
+const MONGO_URL = process.env.MONGO_URL;
 
 // mongodb://localhost:27017/
 // DB connect

68bf68f20edc69421946098463ebe9c08efe0d3f
Akshay More <moreakshay725@gmail.com>
2026-01-30 21:18:20 +0530

    multiple test cases

--- 19__jest/sum.test.js
+++ 19__jest/sum.test.js
@@ -1,5 +1,8 @@
 const sum = require("./sum.js");
 
 test("adds 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(4);
+});
+test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });

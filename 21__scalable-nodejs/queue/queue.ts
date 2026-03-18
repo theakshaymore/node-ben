c7b3ec6ec4b9f221c723cce68699d86b1cc392eb
Akshay More <moreakshay725@gmail.com>
2026-03-18 18:12:29 +0530

    feat: add email processing queue using bullmq

---
+++ 21__scalable-nodejs/queue/queue.ts
@@ -0,0 +1,21 @@
+import { Queue, Worker } from "bullmq";
+import { sendEmail } from "../utils/email.ts";
+
+const connection = {
+  host: "valkey-4dc0338-akshay-9189.f.aivencloud.com",
+  port: 28503,
+  username: "default",
+  password: "AVNS_eWWQfcK8zJSWam9VKTo",
+  tls: {},
+};
+
+export const emailQueue = new Queue("email-queue", { connection });
+
+new Worker(
+  "email-queue",
+  async (job) => {
+    console.log("job received...", job.id);
+    await sendEmail(job.data);
+  },
+  { connection, limiter: { max: 50, duration: 1000 } },
+);

3272eb124fd93877bae4fa429ae1bc3caaa3f3d1
Akshay More <moreakshay725@gmail.com>
2026-03-30 21:57:35 +0530

    feat: implement user registration and login functionality with bcrypt password hashing

--- 23__movie-watchlist/src/controllers/auth.controller.js
+++ 23__movie-watchlist/src/controllers/auth.controller.js
@@ -1,6 +1,42 @@
+import { prisma } from "../config/db.js";
+import bcrypt from "bcryptjs";
+
 const registerUser = async (req, res) => {
-  const body = req.body;
-  res.json(body);
+  const { name, email, password } = req.body;
+
+  try {
+    // check if user exist or not
+    const userExist = await prisma.user.findUnique({ where: { email } });
+    if (userExist) {
+      return res.status(400).json({
+        success: false,
+        error: "Email or password already exist",
+      });
+    }
+
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // insert user
+    const response = await prisma.user.create({
+      data: { name, email, password: hashedPassword },
+    });
+
+    // send response back
+    if (response) {
+      return res.status(200).json({
+        success: true,
+        message: "user register aptly",
+      });
+    }
+  } catch (error) {
+    return res.status(404).json(error);
+  }
 };
 
-export { registerUser };
+const loginUser = async (req, res) => {
+  //
+};
+
+export { registerUser, loginUser };

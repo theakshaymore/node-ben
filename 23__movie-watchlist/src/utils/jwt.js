bb69740190429dbe2dec2a9974a4ba7a055a52ee
Akshay More <moreakshay725@gmail.com>
2026-03-30 22:27:50 +0530

    jwt secret changes

--- 23__movie-watchlist/src/utils/jwt.js
+++ 23__movie-watchlist/src/utils/jwt.js
@@ -1,16 +1,16 @@
 import jwt from "jsonwebtoken";
 
-const SECRET = "something";
-
-export async function generateToken(user) {
+export function generateToken(user) {
   try {
     const payload = {
       id: user.id,
       email: user.email,
     };
-    const token = jwt.sign(payload, SECRET, { expiresIn: "7d" });
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
     return token;
   } catch (error) {
-    return "jwt error";
+    throw new Error("JWT generation failed");
   }
 }
